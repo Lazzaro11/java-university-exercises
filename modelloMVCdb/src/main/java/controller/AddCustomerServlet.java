259019ade0912914b9e0a304f97eaca254b60bd6
Lazzaro11 <lazzaro.vozza@gmail.com>
2026-06-26 19:16:41 +0200

    Add MVC project with MYSQL integration

--- modelloMVCdb/src/main/java/controller/AddCustomerServlet.java
+++ modelloMVCdb/src/main/java/controller/AddCustomerServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class AddCustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: Elabora la richiesta
+
     }
 
     @Override
